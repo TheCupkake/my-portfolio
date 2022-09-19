323ab15737abbcf9e91557bc059e3b10874f43c1
unknown <abu.eyaaaad@gmail.com>
2022-09-19 22:05:19 +0200

    finished-project

---
+++ frontent_react/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.scss'
+
+import {About, Work, Testimonial, Footer, Header, Skills} from './container'
+import { Navbar } from './components'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
